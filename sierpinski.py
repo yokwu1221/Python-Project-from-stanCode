d99f6cca6324e12fc7c2b892352f8ef469cb6f93
yokwu1221 <yokwu1221@gmail.com>
2025-10-20 21:15:15 +0800

    Add files via upload

---
+++ sierpinski.py
@@ -0,0 +1,68 @@
+"""
+File: sierpinski.py
+Name: Yok
+---------------------------
+This file recursively prints the Sierpinski triangle on GWindow.
+The Sierpinski triangle is a fractal described in 1915 by Waclaw Sierpinski.
+It is a self similar structure that occurs at different levels of iterations.
+"""
+
+from campy.graphics.gwindow import GWindow
+from campy.graphics.gobjects import GLine
+from campy.gui.events.timer import pause
+
+# Constants
+ORDER = 6                  # Controls the order of Sierpinski Triangle
+LENGTH = 600               # The length of order 1 Sierpinski Triangle
+UPPER_LEFT_X = 150		   # The upper left x coordinate of order 1 Sierpinski Triangle
+UPPER_LEFT_Y = 100         # The upper left y coordinate of order 1 Sierpinski Triangle
+WINDOW_WIDTH = 950         # The width of the GWindow
+WINDOW_HEIGHT = 700        # The height of the GWindow
+
+# Global Variable
+window = GWindow(width=WINDOW_WIDTH, height=WINDOW_HEIGHT)  # The canvas to draw Sierpinski Triangle
+
+
+def main():
+	sierpinski_triangle(ORDER, LENGTH, UPPER_LEFT_X, UPPER_LEFT_Y)
+
+
+def sierpinski_triangle(order, length, upper_left_x, upper_left_y):
+	"""
+	:param order: the number of triangle
+	:param length: the length of triangle
+	:param upper_left_x: the upper_left x coordinate point of triangle
+	:param upper_left_y: the upper left y coordinate point of triangle
+	"""
+	line1 = GLine(upper_left_x, upper_left_y, 0.5*length+upper_left_x, 0.866*length+upper_left_y)
+	line2 = GLine(upper_left_x, upper_left_y, length+upper_left_x, upper_left_y)
+	line3 = GLine(0.5*length+upper_left_x, 0.866*length+upper_left_y, length+upper_left_x, upper_left_y)
+	window.add(line1)
+	window.add(line2)
+	window.add(line3)
+	sierpinski_triangle_helper(order, length, upper_left_x, upper_left_y)
+
+
+def sierpinski_triangle_helper(order, length, upper_left_x, upper_left_y):
+	if order == 1:
+		pass
+	else:
+		line1 = GLine(length/4+upper_left_x, 0.866*length/2+upper_left_y, length/2+upper_left_x, upper_left_y)
+		line2 = GLine(length/4+upper_left_x, 0.866*length/2+upper_left_y, 3*length/4+upper_left_x, 0.866*length/2+upper_left_y)
+		line3 = GLine(length/2+upper_left_x, upper_left_y, 3*length/4+upper_left_x, 0.866*length/2+upper_left_y)
+		window.add(line1)
+		window.add(line2)
+		window.add(line3)
+		# # upper left
+		sierpinski_triangle_helper(order-1, length/2, upper_left_x, upper_left_y)
+
+		# upper right
+		sierpinski_triangle_helper(order-1, length/2, upper_left_x+(length/2), upper_left_y)
+
+		# # lower
+		sierpinski_triangle_helper(order-1, length/2, upper_left_x+(length/4), upper_left_y+(0.866*length/2))
+
+
+
+if __name__ == '__main__':
+	main()
